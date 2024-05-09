78f99113ee1d2ff9328012fae8022e14776d2c0c
Chandra Prakash Sharma <yogicinfotech@gmail.com>
2024-05-10 04:50:17 +0530

    Saved

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -99,8 +99,17 @@ export default function Home() {
         </div>
       </div>
 
+
+      
+
+  
+
+
       {/* Features Section */}
       <div className="mx-auto my-32 max-w-7xl px-2 lg:px-8">
+      
+
+
         <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4">
           <div>
             <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
