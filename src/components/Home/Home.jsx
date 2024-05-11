ffd7cb2cb02c6674f563a3e52470dadf28a5b261
Chandra Prakash Sharma <153484592+chandraprakashsarma@users.noreply.github.com>
2024-05-10 20:08:31 -0700

    Mu Image Updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import myImage from '../../assets/myImage.jpg';
 
 
 export default function Home() {
@@ -40,11 +40,10 @@ export default function Home() {
             </form>
           </div>
           <div className="relative lg:col-span-5 lg:-mr-8 xl:col-span-6">
-            <img
-              className="aspect-[3/2] bg-gray-50 object-cover lg:aspect-[4/3] lg:h-[700px] xl:aspect-[16/9]"
-              src="https://plus.unsplash.com/premium_photo-1679079456783-5d862f755557?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjQ3fHxtYW4lMjB3aXRoJTIwbGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt=""
-            />
+            
+            <img src={myImage} alt="My Image" className="aspect-[3/2] bg-gray-50 object-cover lg:aspect-[4/3] lg:h-[700px] xl:aspect-[16/9]" />
+          
+
           </div>
         </div>
       </div>
