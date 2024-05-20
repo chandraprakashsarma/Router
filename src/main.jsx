00b40ce01eed61031dd32fdffb9fb7974b6583dd
Chandra Prakash Sharma <153484592+chandraprakashsarma@users.noreply.github.com>
2024-05-19 22:47:22 -0700

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -160,3 +160,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+{/*
+
+<h1 className="mt-8 mb-6 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-2xl">
+     List of products, and users can add new products to the list.
+</h1>
+<div className="flex items-center justify-center h-96 bg-gray-100">
+<div className="container mx-auto w-1/2 bg-white p-8 rounded shadow-lg">
+        
+</div>
+</div>
+
+*/}
