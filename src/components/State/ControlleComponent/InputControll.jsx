418cc520002a51fd2f1477c07219163c752eed1f
Chandra Prakash Sharma <153484592+chandraprakashsarma@users.noreply.github.com>
2024-05-20 00:22:18 -0700

    Updated Theme Swicher Tailwind setting

--- src/components/State/ControlleComponent/InputControll.jsx
+++ src/components/State/ControlleComponent/InputControll.jsx
@@ -314,9 +314,7 @@ const InputControll = () => {
         Register
       </button>
     </form>
-    <h1 className="mt-8 mb-6 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-2xl">
-        {formData} 
-    </h1>
+    
     </div>
     
     
