da3c6327d5d9ffe5cbeedb097d4bfc5061a75336
Chandra Prakash Sharma <153484592+chandraprakashsarma@users.noreply.github.com>
2024-06-19 02:47:12 -0700

    push all use

--- src/components/State/State Lifting/StateLifting.jsx
+++ src/components/State/State Lifting/StateLifting.jsx
@@ -21,6 +21,16 @@ function StateLifting() {
             <YoutubApp title ="Node JS Tutorial"views = "1M"/>
             
         </div></div>
+
+        <h1 className="mt-8 mb-6 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-2xl">
+            YouTube Video App
+        </h1>
+        <div className="flex items-center justify-center h-96 bg-gray-100">
+        <div className="container mx-auto w-1/2 bg-white p-8 rounded shadow-lg">
+            <YoutubApp title ="React JS Tutorial" views = "10K"/>
+            <YoutubApp title ="Node JS Tutorial"views = "1M"/>
+            
+        </div></div>
 +-
     </>
   )
